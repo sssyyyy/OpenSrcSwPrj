93583a3bef1a963c572310aa92d9af202b98720b
thdud <thdud4548@naver.com>
2020-04-23 03:43:01 +0900

    gugudan version-1.0 if input num, output num's gugudan

--- Gugudan.java
+++ Gugudan.java
@@ -7,18 +7,8 @@ public class Gugudan {
 		Scanner scn = new Scanner(System.in);
 		int num=scn.nextInt();
 		
-		if(num==0)
-		{
-			for(int i=2;i<10;i++)
-			{
-				System.out.println(i+"단");
-				for(int j=1;j<10;j++)
-					System.out.println(i+" * "+j+" = "+i*j);
-			}	
-		}
-		else
-		{
 		for(int i=1;i<10;i++)
+		{
 			System.out.println(num+" * "+i+" = "+num*i);
 		}
 
