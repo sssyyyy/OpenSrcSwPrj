4a16311d3ae66e259d26219d737837f720e41b9f
thdud <thdud4548@naver.com>
2020-04-23 03:39:19 +0900

    gugudan version-2.0 if input minus, output print error message

---
+++ Gugudan.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		System.out.print("출력하고 싶은 단을 입력하시오: ");
+		Scanner scn = new Scanner(System.in);
+		int num=scn.nextInt();
+		
+		if(num==0)
+		{
+			for(int i=2;i<10;i++)
+			{
+				System.out.println(i+"단");
+				for(int j=1;j<10;j++)
+					System.out.println(i+" * "+j+" = "+i*j);
+			}	
+		}
+		else if(num>0)
+		{
+		for(int i=1;i<10;i++)
+			System.out.println(num+" * "+i+" = "+num*i);
+		}
+		else
+		{
+			System.out.println("구구단 출력이 불가 합니다.");
+		}
+
+	}
+
+}
